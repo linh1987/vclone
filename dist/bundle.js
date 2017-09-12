fdb16da78b49439a2937a55a9acfb1154f8feffe
linh1987 <linh1987@gmail.com>
2017-09-12 10:19:17 +0700

    crude implementation

--- dist/bundle.js
+++ dist/bundle.js
@@ -5,13 +5,41 @@ var vclone = (function (exports) {
 function empty(node) {
     [].slice.call(node.childNodes).forEach(node.removeChild, node);
 }
+//# sourceMappingURL=DOM.js.map
 
+var VNode = (function () {
+    function VNode(dom) {
+        console.log('creating ' + JSON.stringify(dom));
+        this.renderedDom = dom;
+    }
+    return VNode;
+}());
 var createElement = function (tag, props, children) {
-    console.log('createElement: ' + tag);
-    throw "Not implemented";
+    if (typeof tag === 'string') {
+        var renderedDOM_1 = null;
+        console.log('creating [' + tag + ']');
+        renderedDOM_1 = document.createElement(tag);
+        if (children && children.forEach) {
+            children.forEach(function (child) {
+                if (typeof child === 'string') {
+                    var wrapper = document.createElement('span');
+                    wrapper.textContent = child;
+                    renderedDOM_1.appendChild(wrapper);
+                }
+                else {
+                    renderedDOM_1.appendChild(child.renderedDom);
+                }
+            });
+        }
+        return new VNode(renderedDOM_1);
+    }
+    else if (typeof tag === 'function') {
+        return tag();
+    }
 };
 var render = function (vdom, el) {
     empty(el);
+    el.appendChild(vdom.renderedDom);
 };
 
 exports.createElement = createElement;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
 import * as DOM from './DOM'
 
 class VNode {
+    
+    renderedDom: Element
 
+    constructor(dom : Element) {
+        console.log('creating ' + JSON.stringify(dom));
+        this.renderedDom = dom;
+    }
 }
 
-export var createElement = function(tag: string, props: any, children: VNode[]) : VNode {
-    console.log('createElement: ' + tag);
-    throw "Not implemented";
-} 
+export const createElement = function (tag: string | Function, props: any, children: (VNode| string)[]): VNode {
+    if (typeof tag === 'string') {
+        let renderedDOM: Element = null;
+        console.log('creating [' + tag + ']');
+        renderedDOM = document.createElement(tag);
 
-export var render = function(vdom : VNode, el: Element) {
+        if (children && children.forEach) {
+            children.forEach((child) => {
+                if (typeof child === 'string') {
+                    const wrapper = document.createElement('span');
+                    wrapper.textContent = child
+                    renderedDOM.appendChild(wrapper)
+                } else {
+                    renderedDOM.appendChild(child.renderedDom);
+                }
+            })
+        }
+
+        return new VNode(renderedDOM);
+    }
+    else if (typeof tag === 'function') {
+        return tag();
+    }
+}
+
+export const render = function (vdom: VNode, el: Element) {
     DOM.empty(el);
+    el.appendChild(vdom.renderedDom);
 }
