cf00dd99a2f56305bc089e16c97f1c1797427c93
linh1987 <linh1987@gmail.com>
2017-09-12 17:09:00 +0700

    update Component test cases

--- dist/bundle.js
+++ dist/bundle.js
@@ -16,24 +16,21 @@ var createElement = function (tag, props, children) {
     var renderedDOM = null;
     if (typeof tag === 'string') {
         renderedDOM = document.createElement(tag);
+        if (typeof children === 'string')
+            children = [children];
         if (children) {
-            if (typeof children === 'string') {
-                var spanWrapper = document.createElement('span');
-                spanWrapper.textContent = children;
-                renderedDOM.appendChild(spanWrapper);
-            }
-            else {
-                children.forEach(function (child) {
-                    if (typeof child === 'string') {
-                        var spanWrapper = document.createElement('span');
-                        spanWrapper.textContent = child;
-                        renderedDOM.appendChild(spanWrapper);
-                    }
-                    else {
-                        renderedDOM.appendChild(child.renderedDOM);
-                    }
-                });
-            }
+            children.forEach(function (child) {
+                var el = null;
+                if (typeof child === 'string') {
+                    var spanWrapper = document.createElement('span');
+                    spanWrapper.textContent = child;
+                    el = spanWrapper;
+                }
+                else {
+                    el = child.renderedDOM;
+                }
+                renderedDOM.appendChild(el);
+            });
         }
         return new VNode(renderedDOM);
     }
